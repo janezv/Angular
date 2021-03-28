599a3e937b9c9965b241c35a24d94b202679dfef
janezv <janez.vegan@gmail.com>
2021-03-28 13:21:20 +0200

    Dodan Card planine

---
+++ src/app/planina/planina.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planina',
+  templateUrl: './planina.component.html',
+  styleUrls: ['./planina.component.css']
+})
+export class PlaninaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
