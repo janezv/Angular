d6f1aeb5ef4588adff42d3572bb853351890082f
janezv <janez.vegan@gmail.com>
2021-04-11 21:33:04 +0200

    Dodana ena planina

---
+++ src/app/Models/Planina.ts
@@ -0,0 +1,6 @@
+export class Planina{
+  Id:String | undefined;
+  Title:String | undefined;
+  Text:String | undefined;
+  Url:String | undefined;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaninaComponent } from './Planina/planina/planina.component';
+import { PlaninaComponent } from './Planina/planina.component';
 
 @NgModule({
   declarations: [
